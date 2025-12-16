d889cffb6ee6652f0c0c5d76131b108c793f6a28
MaximilianBossauer <maximilian.joel.bossauer@gmail.com>
2025-12-16 16:38:31 +0100

    --

--- Praktomat/FibonacciCalc/src/FibonacciCalculator.c
+++ Praktomat/FibonacciCalc/src/FibonacciCalculator.c
@@ -1,24 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int FibonacciCalculator(int FibonacciNumbers[], int Input, int Count);
+int FibonacciCalculator(int FibonacciNumbers[], int Input);
 int UserInput(int Input);
+int Output(int FibonacciNumbers[], int Input);
 
 int main(void)
 {
+    int SumEven;
+    int SumUneven;
     int Input = UserInput(Input);
-    int Count = Input;
     int FibonacciNumbers[Input];
-    FibonacciCalculator(FibonacciNumbers, Input, Count);
-    for (Count = 1; Count < Input; Count++)
-    {
-        printf("%d\n", FibonacciNumbers[Count]);
-        int NextNumber = FibonacciNumbers[Count] + FibonacciNumbers[Count - 1];
-        if (NextNumber > Input)
-        {
-            break;
-        }
-    }
+    FibonacciCalculator(FibonacciNumbers, Input);
+
+    Output(FibonacciNumbers, Input);
 }
 
 int UserInput(int Input)
@@ -27,25 +22,43 @@ int UserInput(int Input)
     scanf("%d", &Input);
     return Input;
 }
-int FibonacciCalculator(int FibonacciNumbers[], int Input, int Count)
+int FibonacciCalculator(int FibonacciNumbers[], int Input)
 {
-    if (Input > 0)
-    {
-        FibonacciNumbers[0] = 0;
-    }
-    if (Input > 1)
-    {
-        FibonacciNumbers[1] = 1;
-    }
+    int NextNumber = 0;
+    FibonacciNumbers[0] = 0;
+    FibonacciNumbers[1] = 1;
 
-    for (Count = 2; Count < Input; Count++)
+    for (int Count = 2; NextNumber <= Input; Count++)
     {
         FibonacciNumbers[Count] = FibonacciNumbers[Count - 1] + FibonacciNumbers[Count - 2];
-        int NextNumber = FibonacciNumbers[Count] + FibonacciNumbers[Count - 1];
+        NextNumber = FibonacciNumbers[Count] + FibonacciNumbers[Count - 1];
         if (NextNumber > Input)
         {
             break;
         }
     }
-    return Count;
+}
+
+int Output(int FibonacciNumbers[], int Input)
+{
+    int NextNumber = 0;
+    int i = 2;
+    int SumEven = 0;
+    int SumUneven = 0;
+    while (NextNumber <= Input)
+    {
+        if ((FibonacciNumbers[i] % 2) == 1)
+        {
+            SumUneven += FibonacciNumbers[i];
+        }
+        else
+        {
+            SumEven += FibonacciNumbers[i];
+        }
+        NextNumber = FibonacciNumbers[i] + FibonacciNumbers[i - 1];
+        i++;
+    }
+    printf("The sum of all Fibonacci-numbers not exceeding the entered max of %d is %d (even) "
+           "and %d (odd)",
+           Input, SumEven, SumUneven);
 }
