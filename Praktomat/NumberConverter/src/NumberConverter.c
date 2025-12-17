08bdb009d134cde6ae6f4c08f0576185c28e5c67
MaximilianBossauer <maximilian.joel.bossauer@gmail.com>
2025-12-17 11:35:48 +0100

    --

--- Praktomat/NumberConverter/src/NumberConverter.c
+++ Praktomat/NumberConverter/src/NumberConverter.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int UserNumber;
-int hundrets;
-int tens;
-int ones;
-char *words[] = {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
+int UserInput();
+void Extract(int UserNumber, int *hundrets, int *tens, int *ones);
+int ConvertandOutput(int UserNumber,int hundrets, int tens, int ones, char *words[]);
+
+
+
+int main(void)
+{
+   char *words[] = {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
+   int UserNumber;
+   int hundrets, tens, ones;
+   UserNumber = UserInput();
+   Extract(UserNumber, &hundrets, &tens, &ones);
+   ConvertandOutput(UserNumber, hundrets, tens, ones, words);
+   return (EXIT_SUCCESS);
+}
 
 int UserInput(void)
 {
+   int UserNumber;
    printf("Please enter a number between 0 and 999: ");
    scanf("%d", &UserNumber);
    if (UserNumber < 0 || UserNumber > 999)
@@ -16,16 +28,17 @@ int UserInput(void)
       printf("INVALID, NUMBER MUST BE BETWEEN  0 and 999\nTry again\n");
       exit(EXIT_SUCCESS);
    }
+   return UserNumber;
 }
 
-int Extract(void)
+void Extract(int UserNumber, int *hundrets, int *tens, int *ones)
 {
-   hundrets = UserNumber / 100;
-   tens = (UserNumber - hundrets * 100) / 10;
-   ones = UserNumber - hundrets * 100 - tens * 10;
+   *hundrets = UserNumber / 100;
+   *tens = (UserNumber - *hundrets * 100) / 10;
+   *ones = UserNumber - *hundrets * 100 - *tens * 10;
 }
 
-int ConvertandOuput(void)
+int ConvertandOutput(int UserNumber, int hundrets, int tens, int ones, char *words[])
 {
    if (UserNumber >= 100)
    {
@@ -37,12 +50,4 @@ int ConvertandOuput(void)
       printf("%s ", words[tens]);
    }
    printf("%s ", words[ones]);
-}
-
-int main(void)
-{
-   UserInput();
-   Extract();
-   ConvertandOuput();
-   return(EXIT_SUCCESS);
 }
