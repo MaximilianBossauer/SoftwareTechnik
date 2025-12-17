0459a19e4a31a5e84e40225e30f040a49db5bf1a
MaximilianBossauer <maximilian.joel.bossauer@gmail.com>
2025-12-17 12:23:18 +0100

    --

--- Praktomat/PrimeCalculator/src/PrimeCalculator.c
+++ Praktomat/PrimeCalculator/src/PrimeCalculator.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int UserInput(void);
+long long UserInput(void);
+void PrimeFactors(long long Input);
 
 int main(void)
 {
-    UserInput();
+    long long Input = UserInput();
+    printf("%lld", Input);
+    PrimeFactors(Input);
 }
 
-int UserInput(void)
+long long UserInput(void)
 {
-    int Input = 0;
+    long long Input = 0;
     printf("Please enter the number which should be factorized: ");
-    scanf("%d", &Input);
+    scanf("%lld", &Input);
+    return Input;
+}
+
+void PrimeFactors(long long Input)
+{
+    long long Number = Input;
+    while (Number % 2 == 0)     /*If User entered an even Number, which cant be a Prime, divide it by two until it isnt even anymore*/
+    {
+        Number = Number / 2;
+    }
+    printf("\nug %lld", Number);
+for (int n = 3; n*n < Number; n = n+2)
+{
+    while (Number % n == 0)
+    {
+       Number = Number / n;
+    }
+    
+}
+ printf("\nfer %lld", Number);
+
 }
