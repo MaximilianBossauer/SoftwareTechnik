7ee235b327b11359c8c1cb01b4c1e9310ed58e04
MaximilianBossauer <maximilian.joel.bossauer@gmail.com>
2025-12-17 15:08:12 +0100

    --

--- Praktomat/FlightDistance/src/FlightDistance.c
+++ Praktomat/FlightDistance/src/FlightDistance.c
@@ -1,7 +1,53 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
-int main (void){
+void UserInput(void);
+double DegreeCalculation(double Degree, double Minutes, double Seconds);
 
-    printf("HALLO");
+int main(void)
+{
+    double Degree;
+     double Minutes;
+      double Seconds;
+      double deplatDecimal,deplongDecimal,arrlatDecimal,arrlatDecimal;
+    UserInput();
+    DegreeCalculation(Degree, Minutes, Seconds);
+    printf("%lf",deplatDecimal);
+}
+
+void UserInput(void)
+{
+    struct Coordinate
+    {
+        double Degree;
+        double Minutes;
+        double Seconds;
+    };
+    char Symbols[16];
+    printf("Please enter latitude in ° ' '' and longitude in ° ' '' for departure and arrival:");
+    struct Coordinate deplat, deplong, arrlat, arrlong;
+    scanf("%lf%c%lf%c%lf%c%c%lf%c%lf%c%lf%c%c%lf%c%lf%c%lf%c%c%lf%c%lf%c%lf%c%c",
+          &deplat.Degree, &Symbols[0],
+          &deplat.Minutes, &Symbols[1],
+          &deplat.Seconds, &Symbols[2], &Symbols[12],
+          &deplong.Degree, &Symbols[3],
+          &deplong.Minutes, &Symbols[4],
+          &deplong.Seconds, &Symbols[5], &Symbols[13],
+          &arrlat.Degree, &Symbols[6],
+          &arrlat.Minutes, &Symbols[7],
+          &arrlat.Seconds, &Symbols[8], &Symbols[14],
+          &arrlong.Degree, &Symbols[9],
+          &arrlong.Minutes, &Symbols[10],
+          &arrlong.Seconds, &Symbols[11], &Symbols[15]);
+    double deplatDecimal = DegreeCalculation(deplat.Degree, deplat.Minutes, deplat.Seconds);
+    double deplongDecimal = DegreeCalculation(deplong.Degree, deplong.Minutes, deplong.Seconds);
+    double arrlatDecimal = DegreeCalculation(arrlat.Degree, arrlat.Minutes, arrlat.Seconds);
+    double arrlongDecimal = DegreeCalculation(arrlong.Degree, arrlong.Minutes, arrlong.Seconds);
+return deplatDecimal,deplongDecimal,arrlatDecimal,arrlongDecimal;
+}
+
+double DegreeCalculation(double Degree, double Minutes, double Seconds)
+{
+    return Degree + Minutes / 60 + Seconds / 3600;
 }
