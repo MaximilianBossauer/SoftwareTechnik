5233eb338d54579ca484561b1c44fbccd5189853
MaximilianBossauer <maximilian.joel.bossauer@gmail.com>
2025-12-18 17:09:00 +0100

    --

--- Praktomat/Digits/src/Digits.c
+++ Praktomat/Digits/src/Digits.c
@@ -2,28 +2,42 @@
 #include <stdlib.h>
 
 long long UserInput(long long Input);
+long long PowerOf(long long Base, long long Exponent);
 long long Calculation(long long Input, long long sum);
 
-long long main (void){
-    long long sum = 0;
+long long main(void)
+{
+    long long sum = 0, Base, Exponent;
     long long Input = UserInput(Input);
-    sum = Calculation (Input, sum);
-    printf ("%d\n", Input);
-    printf ("%d",sum);
+    sum = Calculation(Input, sum);
+    printf("%lld\n", Input);
+    printf("%lld", sum);
+    return 0;
 }
 
 long long UserInput(long long Input)
 {
-    printf ("Please enter the highest value, which will be selected:");
-    scanf("%d", &Input);
+    printf("Please enter the highest value, which will be selected:");
+    scanf("%lld", &Input);
     return Input;
 }
 
-long long Calculation (long long Input, long long sum){
+long long Calculation(long long Input, long long sum)
+{
     long long sumhelp;
-    for (long long count = 1; count <= Input; count ++){
-        sumhelp = count ;
-        sum += sumhelp;
+    for (int Count = 1; Count <= Input; Count++)
+    {
+        sum += PowerOf(Count, Count);
     }
     return sum;
+}
+
+long long PowerOf(long long Base, long long Exponent)
+{
+    long long Result = 1;
+    for (int i = 0; i < Exponent; i++)
+    {
+        Result *= Base;
+    }
+    return Result;
 }
