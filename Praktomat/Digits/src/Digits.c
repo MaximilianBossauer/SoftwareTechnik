4f2ff5156a632cbe625d0f207895257a16f6676f
MaximilianBossauer <maximilian.joel.bossauer@gmail.com>
2025-12-18 16:46:16 +0100

    --

--- Praktomat/Digits/src/Digits.c
+++ Praktomat/Digits/src/Digits.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int UserInput(int Input);
+long long UserInput(long long Input);
+long long Calculation(long long Input, long long sum);
 
-
-int main (void){
-    int Input = UserInput(Input);
-    printf("%d", Input);
+long long main (void){
+    long long sum = 0;
+    long long Input = UserInput(Input);
+    sum = Calculation (Input, sum);
+    printf ("%d\n", Input);
+    printf ("%d",sum);
 }
 
-int UserInput(int Input)
+long long UserInput(long long Input)
 {
-    printf("Please enter the highest value, which will be selected:");
+    printf ("Please enter the highest value, which will be selected:");
     scanf("%d", &Input);
     return Input;
+}
+
+long long Calculation (long long Input, long long sum){
+    long long sumhelp;
+    for (long long count = 1; count <= Input; count ++){
+        sumhelp = count ;
+        sum += sumhelp;
+    }
+    return sum;
 }
