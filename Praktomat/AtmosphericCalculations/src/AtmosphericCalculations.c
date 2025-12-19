087bf792e9e5bcb3194b554ec27157236c608279
MaximilianBossauer <maximilian.joel.bossauer@gmail.com>
2025-12-19 10:19:49 +0100

    --

--- Praktomat/AtmosphericCalculations/src/AtmosphericCalculations.c
+++ Praktomat/AtmosphericCalculations/src/AtmosphericCalculations.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define Gravity = 9.81;
+#define GasConstant = 287;
+#define PolytropicExponent = 1.235;
+#define TempStandardTrop = 288.15;
+#define PhiStandardTrop = 1.225;
+
 int main (void){
+
 }
