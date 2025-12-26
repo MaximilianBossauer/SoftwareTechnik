fd4d0547b3cd05d23df327c43e5a1fc196ed6039
MaximilianBossauer <maximilian.joel.bossauer@gmail.com>
2025-12-26 11:19:43 +0100

    --

--- Praktomat/FibonacciCalc/src/FibonacciCalculator.c
+++ Praktomat/FibonacciCalc/src/FibonacciCalculator.c
@@ -1,48 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void FibonacciCalculator(int FibonacciNumbers[], int Input);
-int UserInput(int Input);
-void SumCalculation(int FibonacciNumbers[], int Input, int *SumEven, int *SumOdd);
-void Output (int SumEven, int Sumodd, int Input);
-
+void FibonacciCalculator(long long FibonacciNumbers[], long long Input);
+long long UserInput(void);
+void SumCalculation(long long FibonacciNumbers[], long long Input, long long *SumEven, long long *SumOdd);
+void Output(long long SumEven, long long Sumodd, long long Input);
 int main(void)
 {
-    int SumEven, SumOdd;
-    int Input = UserInput(Input);
-    int FibonacciNumbers[Input];
+    long long SumEven, SumOdd;
+    long long Input = UserInput();
+    long long FibonacciNumbers[Input];
     FibonacciCalculator(FibonacciNumbers, Input);
     SumCalculation(FibonacciNumbers, Input, &SumEven, &SumOdd);
     Output(SumEven, SumOdd, Input);
 }
 
-int UserInput(int Input)
+long long UserInput(void)
+/*Reads the maxim allowed fibonacci value the user wants*/
 {
+    long long Input;
     printf("Please enter the max value for calculating the even- and uneven sum of fibonaccinumbers: ");
-    scanf("%d", &Input);
+    scanf("%lld", &Input);
     return Input;
 }
-void FibonacciCalculator(int FibonacciNumbers[], int Input)
+void FibonacciCalculator(long long FibonacciNumbers[], long long Input)
+/*Generates the fibonacci numbers starting with 0 and stpos when they exceed the user input*/
 {
-    int NextNumber = 0;
+    long long NextNumber = 0;
     FibonacciNumbers[0] = 0;
     FibonacciNumbers[1] = 1;
 
-    for (int Count = 2; NextNumber <= Input; Count++)  /*for-loop to calculate as many fibonacci-numbersn as the Input says*/
+    for (long long Count = 2; NextNumber <= Input; Count++) /*for-loop to calculating fibonacci numbers until they exceed the user input*/
     {
         FibonacciNumbers[Count] = FibonacciNumbers[Count - 1] + FibonacciNumbers[Count - 2];
-        NextNumber = FibonacciNumbers[Count] + FibonacciNumbers[Count - 1]; /*Calculation of following fibonacci-number*/
-        if (NextNumber > Input) /*Break condition of the Loop to save computing power if the next fibonacci-number would exceed the Input*/
+        NextNumber = FibonacciNumbers[Count] + FibonacciNumbers[Count - 1]; /*Calculation of following fibonacci-number for the break condition*/
+        if (NextNumber > Input)                                             /*Break condition of the Loop to save computing power if the next fibonacci-number would exceed the Input*/
         {
             break;
         }
     }
 }
 
-void SumCalculation(int FibonacciNumbers[], int Input, int *SumEven, int *SumOdd)
+void SumCalculation(long long FibonacciNumbers[], long long Input, long long *SumEven, long long *SumOdd)
+/*Differentiates between odd and even fibonacci numbers and calculates the sum of each*/
 {
-    int NextNumber = 0;
-    int i = 2;
+    long long NextNumber = 0;
+    long long i = 1;
     *SumEven = 0;
     *SumOdd = 0;
     while (NextNumber <= Input) /*While-loop cycling through all previously calculated fibonacci-numbers*/
@@ -58,11 +61,13 @@ void SumCalculation(int FibonacciNumbers[], int Input, int *SumEven, int *SumOdd
         NextNumber = FibonacciNumbers[i] + FibonacciNumbers[i - 1]; /*Calculation of the following fibonacci-number to use in the while as break condition*/
         i++;
     }
-
 }
 
-void Output (int SumEven, int SumOdd, int Input){
-    printf("The sum of all Fibonacci-numbers not exceeding the entered max of %d is %d (even) "
-           "and %d (odd)",
+void Output(long long SumEven, long long SumOdd, long long Input)
+/*Prints out the calculated sums and the input*/
+{
+
+    printf("The sum of all Fibonacci-numbers not exceeding the entered max of %lld is %lld (even) "
+           "and %lld (odd)",
            Input, SumEven, SumOdd);
 }
--- Praktomat/NumberConverter/src/NumberConverter.c
+++ Praktomat/NumberConverter/src/NumberConverter.c
@@ -2,26 +2,27 @@
 #include <stdlib.h>
 
 int UserInput();
-void Extract(int UserNumber, int *hundrets, int *tens, int *ones);
-void ConvertandOutput(int UserNumber, int hundrets, int tens, int ones, char *words[]);
+void Extract(int UserNumber, int *hundreds, int *tens, int *ones);
+void ConvertandOutput(int UserNumber, int hundreds, int tens, int ones, char *words[]);
 
 int main(void)
 {
-   char *words[] = {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"}; /*Use of Char for easy call and efficient use*/
+   char *words[] = {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"}; /*Char for easy call and efficient use*/
    int UserNumber;
-   int hundrets, tens, ones;
+   int hundreds, tens, ones;
    UserNumber = UserInput();
-   Extract(UserNumber, &hundrets, &tens, &ones);
-   ConvertandOutput(UserNumber, hundrets, tens, ones, words);
+   Extract(UserNumber, &hundreds, &tens, &ones);
+   ConvertandOutput(UserNumber, hundreds, tens, ones, words);
    return (EXIT_SUCCESS);
 }
 
 int UserInput(void)
+/*Reads the given number and checks if its in the given range 0-999*/
 {
    int UserNumber;
    printf("Please enter a number between 0 and 999: ");
    scanf("%d", &UserNumber);
-   if (UserNumber < 0 || UserNumber > 999) /*If-loop to check if wrong Input was entered*/
+   if (UserNumber < 0 || UserNumber > 999) /*If-statement to check if wrong Input was entered*/
    {
       printf("INVALID, NUMBER MUST BE BETWEEN  0 and 999\nTry again\n");
       exit(EXIT_SUCCESS);
@@ -29,18 +30,20 @@ int UserInput(void)
    return UserNumber;
 }
 
-void Extract(int UserNumber, int *hundrets, int *tens, int *ones) /*Function to convert the given number in three singular numbers*/
+void Extract(int UserNumber, int *hundreds, int *tens, int *ones) 
+/*Splits the given Number into its hundreds, tens and ones digits*/
 {
-   *hundrets = UserNumber / 100; 
-   *tens = (UserNumber - *hundrets * 100) / 10;
-   *ones = UserNumber - *hundrets * 100 - *tens * 10;
+   *hundreds = UserNumber / 100; 
+   *tens = (UserNumber - *hundreds * 100) / 10; 
+   *ones = UserNumber - *hundreds * 100 - *tens * 10;
 }
 
-void ConvertandOutput(int UserNumber, int hundrets, int tens, int ones, char *words[])
+void ConvertandOutput(int UserNumber, int hundreds, int tens, int ones, char *words[])
+/*Converting the singular Digits into words and printing them into the terminal*/
 {
    if (UserNumber >= 100)
    {
-      printf("%s ", words[hundrets]);
+      printf("%s ", words[hundreds]);
    }
    if (UserNumber >= 10)
 
